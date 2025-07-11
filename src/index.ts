dc1936282db5ccd959024e4ec962d6d52ed00275
amosmurmu <amosmurmu002@gmail.com>
2025-07-11 16:57:27 +0530

    impl: basic express index

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,28 @@ dotenv.config()
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   return res.status(200).json({
+//     sucess: 'banana',
+//   })
+// })
 
 app.post('/send-email', async (req, res) => {
   const { to, subject, body, requestId } = req.body
 
   if (!to || !subject || !body || !requestId) {
-    return res.status()
+    return res.status(400).json({
+      error: 'Missing required fields',
+    })
   }
+  const result = 'success'
+  res.json(result)
+})
+
+const port = process.env.PORT
+
+app.listen(port || 8000, () => {
+  console.log(`The server is listening to ${port}`)
 })
